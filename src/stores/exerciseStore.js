2d6e5c585731c6ddc9dd1e7aa0f9dd80a2e2d318
LucaBloise <lbloise@itba.edu.ar>
2023-10-19 04:58:46 -0300

    creacion de exercise y routine stores

--- src/stores/exerciseStore.js
+++ src/stores/exerciseStore.js
@@ -1,19 +1,77 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { ExerciseApi } from '@/api/exercise'
 
 export const useExerciseStore = defineStore('exercise', () => { 
   //  estado
-  
+  const items =  ref([])
 
   //  getters
-  
+  const findIndex = computed((exercise) => {
+      return items.findIndex(item => item.id === exercise.id)
+  })
   
   //  actions
-  
+  function push(exercise) {
+    items.push(exercise);
+  }
+
+  function replace(index, exercise) {
+    items[index] = exercise;
+  }
+
+  function splice(index) {
+    items.splice(index, 1);  
+  }
+
+  function replaceAll(exercises) {
+    items = exercises;
+  }
+
+  async function create(exercise) {
+    const result = await ExerciseApi.addExercise(exercise);
+    if (!findIndex(result))
+        push(result);
+    return result;
+  }
+
+  async function modify(exercise) {
+    const result = await ExerciseApi.modifyExercise(exercise);
+    const index = findIndex(result);
+    if (index >= 0)
+        replace(index, result);
+    return result;
+  }
+
+  async function delete(exercise) {
+    await ExerciseApi.deleteExercise(exercise.id);
+    const index = findIndex(exercise);
+    if (index >= 0)
+        splice(index);
+  }
+
+  async function get(exercise) {
+    const index = findIndex(exercise);
+    if (index >= 0)
+        return items[index];
+
+    const result = await ExerciseApi.getExercise();
+    push(result);
+    return result;
+  }
+
+  async function getAll(controller) {
+    const result = await ExerciseApi.getAllExercises(controller);
+    return result;
+  }
   
 
-  
   return {
-
+      findIndex,
+      create,
+      modify,
+      delete,
+      get,
+      getAll
    }
 })
--- src/stores/routineStore.js
+++ src/stores/routineStore.js
@@ -1,19 +1,78 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { RoutineApi } from '@/api/routine'
 
-export const useRoutineStore = defineStore('routine', () => { 
+export const useroutineStore = defineStore('routine', () => { 
   //  estado
-  
+  const items =  ref([])
 
   //  getters
-  
+  const findIndex = computed((routine) => {
+      return items.findIndex(item => item.id === routine.id)
+  })
   
   //  actions
-  
+  function push(routine) {
+    items.push(routine);
+  }
+
+  function replace(index, routine) {
+    items[index] = routine;
+  }
+
+  function splice(index) {
+    items.splice(index, 1);  
+  }
+
+  function replaceAll(routines) {
+    items = routines;
+  }
+
+  async function createRoutine(routine) {
+    const result = await RoutineApi.addRoutine(routine);
+    if (!findIndex(result))
+        push(result);
+    return result;
+  }
+
+  async function modifyRoutine(routine) {
+    const result = await RoutineApi.modifyRoutine(routine);
+    const index = findIndex(result);
+    if (index >= 0)
+        replace(index, result);
+    return result;
+  }
+
+  async function deleteRoutine(routine) {
+    await RoutineApi.removeRoutine(routine.id);
+    const index = findIndex(routine);
+    if (index >= 0)
+        splice(index);
+  }
+
+  async function getRoutine(routine) {
+    const index = findIndex(routine);
+    if (index >= 0)
+        return items[index];
+
+    const result = await RoutineApi.getRoutine();
+    push(result);
+    return result;
+  }
+
+  async function getAllRoutines(controller) {
+    const result = await RoutineApi.getAllRoutines(controller);
+    return result;
+  }
   
 
-  
+
   return {
-
+    findIndex,
+    create,
+    modify,
+    delete,
+    get,
+    getAll
    }
 })
